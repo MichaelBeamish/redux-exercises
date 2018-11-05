3f1ebb95d5f2a099131c3cf141a4b0bd3038ecdf
Michael Beamish <michaelbeamish89@gmail.com>
2018-11-05 11:49:00 -0700

    YEAH BABYgit add -Agit add -A!

--- redux-1-actions-practice/src/practice/actions/actions-practice.js
+++ redux-1-actions-practice/src/practice/actions/actions-practice.js
@@ -6,14 +6,14 @@
     - An action creator function called "addTodo" that expects a new todo object as an argument.
 */
 
-export const ADD_TODO = 'ADD_TODO'
+export const ADD_TODO = "ADD_TODO";
 
-export const addTodo = (newTodo) => {
+export const addTodo = newTodo => {
   return {
     type: ADD_TODO,
     payload: newTodo
-  }
-}
+  };
+};
 
 /*
   Problem # 1
@@ -23,6 +23,14 @@ export const addTodo = (newTodo) => {
     - An action creator function called "increment" that expects no arguments.
 */
 
+export const INCREMENT = "INCREMENT";
+
+export const increment = () => {
+  return {
+    type: INCREMENT
+  };
+  return INCREMENT;
+};
 
 /*
   Problem # 2
@@ -31,7 +39,15 @@ export const addTodo = (newTodo) => {
     - An action type label that says ADD_GROCERY
     - An action creator function called "addGrocery" that expects one argument "newGroceryItem".
 */
+export const ADD_GROCERY = "ADD_GROCERY";
 
+export const addGrocery = newGroceryItem => {
+  return {
+    type: ADD_GROCERY,
+    payload: newGroceryItem
+  };
+  return addGrocery;
+};
 
 /*
   Problem # 3
@@ -40,7 +56,15 @@ export const addTodo = (newTodo) => {
     - An action type label that says UPDATE_CONTACT
     - An action creator function called "updateContact" that expects one argument "updatedContact".
 */
+export const UPDATE_CONTACT = "UPDATE_CONTACT";
 
+export const updateContact = updatedContact => {
+  return {
+    type: UPDATE_CONTACT,
+    payload: updatedContact
+  };
+  return updateContact;
+};
 
 /*
   Problem # 4
@@ -49,7 +73,15 @@ export const addTodo = (newTodo) => {
     - An action type label that says REMOVE_USER
     - An action creator function called "removeUser" that expects one argument "id".
 */
+export const REMOVE_USER = "REMOVE_USER";
 
+export const removeUser = id => {
+  return {
+    type: REMOVE_USER,
+    payload: id
+  };
+  return removeUser;
+};
 
 /*
   Problem # 5
@@ -58,3 +90,12 @@ export const addTodo = (newTodo) => {
     - An action type label that says FETCH_USER
     - An action creator function called "fetchUser" that expects one argument "id".
 */
+export const FETCH_USER = "FETCH_USER";
+
+export const fetchUser = id => {
+  return {
+    type: FETCH_USER,
+    payload: id
+  };
+  return fetchUser;
+};
